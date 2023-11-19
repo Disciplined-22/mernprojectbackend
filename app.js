12cfff2ff695416c6e87687e50e03e0a5c0281db
sachinrohi <ogamersid@gmail.com>
2023-11-19 23:14:05 +0530

    Your commit message

--- app.js
+++ app.js
@@ -6,15 +6,9 @@ const cors = require("cors");
 const router = require("./Routes/router");
 const PORT = process.env.PORT || 8000;
 
-// Use cors middleware with specific configuration
-const corsOptions = {
-  origin: process.env.CORS_ORIGIN || "*", // Update with your frontend URL in production
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  optionsSuccessStatus: 204,
-};
+;
 
-app.use(cors(corsOptions));
+app.use(cors());
 
 app.use(express.json());
 app.use("/uploads", express.static("./uploads"));
