c8efe6caba3e240d287b19791f492bb766f2c19b
sachinrohi <ogamersid@gmail.com>
2023-11-19 15:49:23 +0530

    app.js changes

--- app.js
+++ app.js
@@ -4,16 +4,24 @@ const app = express();
 require("./DB/db");
 const cors = require("cors");
 const router = require("./Routes/router");
-const PORT = process.env.PORT || 8000
+const PORT = process.env.PORT || 8000;
 
+// Use cors middleware with specific configuration
+const corsOptions = {
+  origin: process.env.CORS_ORIGIN || "*", // Update with your frontend URL in production
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true,
+  optionsSuccessStatus: 204,
+};
 
-app.use(cors());
+app.use(cors(corsOptions));
+
 app.use(express.json());
-app.use("/uploads",express.static("./uploads"));
-app.use("/files",express.static("./public/files"));
+app.use("/uploads", express.static("./uploads"));
+app.use("/files", express.static("./public/files"));
 
 app.use(router);
 
-app.listen(PORT,() =>{
-    console.log(`Server start at port no ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server start at port no ${PORT}`);
+});
